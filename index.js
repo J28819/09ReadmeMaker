e9848e7a22cb81666387c32c5a380ab6af1cedb1
Ismael Leal <ismaelleal@Ismaels-MacBook-Pro.local>
2022-07-09 08:36:27 -0500

    Complete question logs and adding all of them in an JSOn Array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-const inquirer = require('inquirer')
-const chalk = require('chalk')
+const inquirer = require('inquirer');
+import chalk from 'chalk';
 
 console.log( '---------------------------------------------------------\n');
  console.log('\x1b[36m%s\x1b[0m', 'Hi, welcome to Pro Readme Gen v1.0.0 by Ismael Leal   |\n');
@@ -74,46 +74,67 @@ console.log( '---------------------------------------------------------\n');
   },
    {
     type: 'input',
-    name: '',
-    message: "",
-    validate(value) {
-      const pass = value;
-      var list = (pass.split(','))
-      if (list.length >= 3) {
-        return true;
-      }
-      return "\033[0;31m\ "+``;
-    },
+    name: 'Credits',
+    message: "Credits Add a list of your collaborators, if any with links to their GitHub Profiles",
+  },
+  {
+    type: 'list',
+    name: 'License',
+    message: '',
+    choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License",
+  'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0',  'Creative Commons Zero v1.0 Universal',
+  'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1'  ],
+   answerlines: 1,
+   
   },
   {
    type: 'input',
-   name: '',
-   message: "",
+   name: 'Features',
+   message: "If your project has a lot of features, list them here. Add minimum 3 of them\n",
    validate(value) {
      const pass = value;
      var list = (pass.split(','))
      if (list.length >= 3) {
        return true;
      }
-     return "\033[0;31m\ "+``;
+     return "\033[0;31m\ "+`is really important to make your README file looks professional as possible\n please add minimum 3 Features  for your app`;
    },
  },
  {
   type: 'input',
-  name: '',
-  message: "",
+  name: 'Contribute',
+  message: "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you`d prefer.\n",
   validate(value) {
     const pass = value;
     var list = (pass.split(','))
     if (list.length >= 3) {
       return true;
     }
-    return "\033[0;31m\ "+``;
+    return "\033[0;31m\ "+`Please add more than 1 guide lines from your app`;
   },
+},
+,
+{
+  type: 'input',
+  name: 'Test',
+  message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here. \n',
+  choices: [],
 }
  ];
  
  inquirer.prompt(questions).then((answers) => {
+
+    let Title = `${answers.Title}`;
+    let Description = `${answers.Description}`;
+    let TableofContents = `${answers.TableofContents}`;
+    let Installation = `${answers.Installation}`;
+    let ScreenShots = `${answers.ScreenShots}`;
+    let Credits = `${answers.Credits}`;
+    let License = `${answers.License}`;
+    let Features = `${answers.Features}`;
+    let Contribute = `${answers.Contribute}`;
+    let Tests = `${answers.Tests}`;
+    
    console.log('\nOrder receipt:');
    console.log(JSON.stringify(answers, null, '  '));
  });
